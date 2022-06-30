1dc6fabe6b69fd0410c0e4cd889532442ac80879
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2022-06-30 10:59:22 -0300

    Cria arquivo dedicado para chamada do docker run
    Durante a execução do make container, estamos recebendo mensagens de programas
    que não existem no servidor, mas são executadas durante a inicialização do make.
    
    O objetivo de separar a execução do docker run é se livrar dessas mensagens.

---
+++ docker.sh
@@ -0,0 +1,11 @@
+docker run -v /$(PWD):/work_dir \
+                         -v ~/.local/share/AzureR:/root/.local/share/AzureR \
+                         -e CKAN_HOST=$(CKAN_HOST) \
+                         -e CKAN_KEY=$(CKAN_KEY) \
+                         -e HTTPS_PROXY=$(HTTPS_PROXY) \
+                         -e DB_HOST=$(DB_HOST) \
+                         -e DB_USER=$(DB_USER) \
+                         -e DB_DATABASE=$(DB_DATABASE) \
+                         -e DB_PASSWORD=$(DB_PASSWORD) \
+                         -e RETICULATE_PYTHON=$(RETICULATE_PYTHON) \
+                         gabrielbdornas/dtamg-age7:latest bash
