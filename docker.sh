74502d514e339722a6cc7d0e6ab09c8a3e04d267
Gabriel Braico Dornas <gabrielbdornas@gmail.com>
2022-10-18 21:08:57 -0300

    Mount pwd in docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source $HOME/.bash_profile
 docker run -i --rm -v $PROJECT_PATH:/work_dir \
+                         -v /pwd:/pwd \
                          -v $PROJECT_PATH/.local/share/AzureR:/root/.local/share/AzureR \
                          -e CKAN_HOST=$CKAN_HOST \
                          -e CKAN_KEY=$CKAN_KEY \
