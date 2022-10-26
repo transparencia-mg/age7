5494dccffec4bb01ed664dd5594d9a44142f123f
andrelamor <amorim.andrel@gmail.com>
2022-10-26 14:22:46 -0300

    leitura vetor arquivo yaml no script parse-sql.R

--- scripts/r/parse-sql.R
+++ scripts/r/parse-sql.R
@@ -2,5 +2,10 @@ library(dotenv)
 
 resource_name <- commandArgs(trailingOnly = TRUE)
 
-sql_query <- dtamg::parse_sql("datapackage.yaml", gsub("-", "_", resource_name))
+df <- "datapackage.yaml"
+
+df <- as.data.frame(t(df))
+
+sql_query <- dtamg::parse_sql(df, gsub("-", "_", resource_name))
+
 writeLines(sql_query, glue::glue("scripts/sql/{resource_name}.sql"))
