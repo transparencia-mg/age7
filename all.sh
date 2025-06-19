92786f36c680485fb441cc730bba3ca4cb03de81
transparenciacgemg <transparenciacgemg@gmail.com>
2025-06-19 06:14:11 +0000

    Atualização age7 - logs notificação

--- all.sh
+++ all.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
+
+# Detecta número de CPUs disponíveis
+CPU_TOTAL=$(nproc)
+# Reserva 1 CPU para o sistema
+CPU_MAKE=$((CPU_TOTAL - 1))
+# Caso a máquina tenha só 1 CPU, use 1 para não dar erro
+if [ "$CPU_MAKE" -lt 1 ]; then
+  CPU_MAKE=1
+fi
+
 {
-git pull origin main && \
-make clean && \
-make parse && \
-make full-extract && \
-make ingest && \
-make data && \
-make validate && \
-make check-validation && \
-make report && \
-make datapackage.json && \
-make test && \
-make build && \
-make update && \
-git add . && git commit -m "Atualização age7"
+  git pull origin main && \
+  make clean && \
+  make -j $CPU_MAKE parse && \
+  make -j $CPU_MAKE full-extract && \
+  make -j $CPU_MAKE ingest && \
+  make -j $CPU_MAKE data && \
+  make -j $CPU_MAKE validate && \
+  make -j $CPU_MAKE check-validation && \
+  make -j $CPU_MAKE report && \
+  make -j $CPU_MAKE datapackage.json && \
+  make -j $CPU_MAKE test && \
+  make -j $CPU_MAKE build && \
+  make -j $CPU_MAKE update && \
+  git add . && git commit -m "Atualização age7"
 } > logs/all.txt
+
 echo $? > logs/exit-code.txt
+
 git add . && git commit -m "Atualização age7 - logs notificação"
 git push origin main
